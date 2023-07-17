376f41fe020986716343be7b82b57a4da16d39e7
f-murphy <murphy.fariz@gmail.com>
2023-07-18 01:39:53 +0300

    add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+
+import "../../styles/header.scss";
+
+const Header = () => {
+
+    return (
+        <div className="container">
+            <div className="title">
+                JACKJSON
+            </div>
+            <div className="description">
+                The .json translate tool | alpha
+            </div>
+        </div>
+    )
+}
+
+export default Header
