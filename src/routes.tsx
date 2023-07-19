f7a6970a52ebf1a6a3651a057946f38663e2d068
Runar <runarnazmutdinov@gmail.com>
2023-07-19 22:33:47 +0300

    add: configure routing

---
+++ src/routes.tsx
@@ -0,0 +1,38 @@
+
+import { path } from "./utils/constants";
+
+export const routes = [
+  {
+    path: path.AUTH_ROUTE,
+
+  },
+  {
+    path: path.REGISTRATION_ROUTE,
+
+  },
+  {
+    path: path.HOME_ROUTE,
+
+  },
+  {
+    path: path.GAME_ROUTE,
+
+  },
+
+  {
+    path: path.FOLDER_ROUTE,
+
+  },
+  {
+    path: path.FILE_ROUTE,
+
+  },
+  {
+    path: path.TRANSLATE_ROUTE,
+
+  },
+  {
+    path: path.NOT_FOUND,
+
+  },
+];
---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export enum path {
+  AUTH_ROUTE = "/auth",
+  REGISTRATION_ROUTE = "/registration",
+  HOME_ROUTE = "/home",
+  GAME_ROUTE = "/game/:gameId",
+  FILE_ROUTE = "/file/:fileId",
+  FOLDER_ROUTE = "/folder/:folderId",
+  TRANSLATE_ROUTE = "/translate/:fileId",
+  NOT_FOUND = "*",
+}
