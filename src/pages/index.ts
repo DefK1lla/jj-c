351bd5f5f5a3fb8954650fd261de2966c5a2de0d
Runar <runarnazmutdinov@gmail.com>
2023-07-20 20:10:56 +0300

    add: pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
-export { AuthPage } from "./AuthPage";
-export { FilePage } from "./FilePage";
-export { HomePage } from "./HomePage";
-export { GamePage } from "./GamePage";
-export { FolderPage } from "./FolderPage";
-export { TranslatePage } from "./TranslatePage";
+export { AuthPage } from "./AuthPage/AuthPage";
+export { FilePage } from "./FilePage/FilePage";
+export { HomePage } from "./HomePage/HomePage";
+export { GamePage } from "./GamePage/GamePage";
+export { FolderPage } from "./FolderPage/FolderPage";
+export { TranslatePage } from "./TranslatePage/TranslatePage";
