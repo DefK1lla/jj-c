48ea9725c39d517d5d57d277def58c6c056e0cf1
f-murphy <murphy.fariz@gmail.com>
2023-07-24 11:18:01 +0300

    refactor scss and Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import s from "./header.module.scss"
-import "./header.css"
+import React from "react";
+import s from "./header.module.scss";
 
+ 
 const Header = () => {
 //TODO add button display depending on the router
     return (
