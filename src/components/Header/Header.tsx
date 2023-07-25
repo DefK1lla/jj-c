6672676c2fdd45dfeb0ed807c757556c6c49a93b
Sedrak Adoyan <set.adoyan@gmail.com>
2023-07-25 17:05:04 +0400

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,13 @@
-import s from "./header.module.scss"
-import "./header.css"
+import s from './header.module.scss'
 
 const Header = () => {
-//TODO add button display depending on the router
-    return (
-        <div className={s.container}>
-            <div className={s.title}>
-                JACKJSON
-            </div>
-            <div className={s.description}>
-                The .json translate tool | alpha
-            </div>
-        </div>
-    )
+  //TODO add button display depending on the router
+  return (
+    <div className={s.container}>
+      <div className={s.title}>JACKJSON</div>
+      <div className={s.description}>The .json translate tool | alpha</div>
+    </div>
+  )
 }
 
 export default Header
