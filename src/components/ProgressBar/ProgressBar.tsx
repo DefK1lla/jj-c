132c6882e1d9293e476698586e2455265c96bf35
R-serban <nurdavletkr@gmail.com>
2023-08-14 21:49:52 +0500

    folder-page

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -10,13 +10,23 @@ export const ProgressBar: FC<ProgressBarProps> = ({
     percent,
     name
 }) => {
+    let color: string = "#FFFFFF"
+    if (percent < 30) {
+        color = "#FF5C00";
+    } else if (percent < 50) {
+        color = "#FFA800"
+    } else if (percent < 90) {
+        color = "#EBFF00";
+    } else {
+        color = "#70FF00";
+    }
     return (
         <div className={q.container} >
-            <div className={q.progress} style={{width: `${percent}%`}}>
-                <div>
+            <div className={q.progress} style={{width: `${percent}%`, backgroundColor: color}}>
+                <div className={q.name}>
                     {name} -
                 </div>
-                <div>
+                <div className={q.percent}>
                 {percent}%
                 </div>
             </div>
